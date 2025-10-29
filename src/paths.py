763354c4cdc2290b4741f2a93ad4ed7e9544ba75
vans-codelab <201007980+vans-codelab@users.noreply.github.com>
2025-10-29 14:37:16 +0100

    Optimise paths.py

--- src/paths.py
+++ src/paths.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 CURRENT_FILE = Path(__file__).resolve()
-ROOT_DIR = CURRENT_FILE.parent.parent
+ROOT_DIR = CURRENT_FILE.parents[1]
 
 LOG_FOLDER = ROOT_DIR / "tests" / "logs"
 SCREENSHOT_FOLDER = ROOT_DIR / "tests" / "screenshots"
